ad53ec2d211d4bec92e655f856b2d9a257023dcc
floov_ubu <kostya.voropaev1998@mail.ru>
2025-10-23 21:26:38 +0300

    commit 5 contact 2

--- src/assets/layout/footer/Footer.tsx
+++ src/assets/layout/footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     return (
         <FooterStyled>
             <Container>
-                <Copyright>Madelyn Torff 202123 </Copyright>
+                <Copyright>Madelyn Torff 2021232 </Copyright>
             </Container>
         </FooterStyled>
     )
