17952f1c786f372fdba1a2de9a3a2ba45f8f5170
floov_ubu <kostya.voropaev1998@mail.ru>
2025-10-24 21:22:12 +0300

    commit 8 footer

--- src/assets/layout/footer/Footer.tsx
+++ src/assets/layout/footer/Footer.tsx
@@ -1,16 +1,27 @@
 import styled from "styled-components";
 import {Container} from "../../../components/Conteiner.styled.tsx";
+import footer from "../../images/footer.svg"
+import {FlexWrapper} from "../../../components/FlexWrapper.styled.tsx";
 
 export const Footer = () => {
     return (
         <FooterStyled>
             <Container>
-                <Copyright>Madelyn Torff 2021 </Copyright>
+                <FlexWrapper>
+                    <Copyright>Madelyn Torff 2021 </Copyright>
+                    <FooterImg src={`${footer}`} alt="footer"/>
+                </FlexWrapper>
+
             </Container>
         </FooterStyled>
     )
 }
 
 const FooterStyled = styled.footer`
+    text-align: center;
+    padding-top: 32px;
 `
-const Copyright = styled.small``
+const Copyright = styled.small`
+`
+const FooterImg = styled.img`
+    `
